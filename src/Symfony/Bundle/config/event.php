5e9a26539d9ced5b05a3488b4dbb6206c394287c
Joel Wurtz <joel.wurtz@gmail.com>
2024-03-19 19:39:45 +0100

    feat(map-to): add listeners to bundle

--- src/Symfony/Bundle/config/event.php
+++ src/Symfony/Bundle/config/event.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use AutoMapper\Event\GenerateMapperEvent;
 use AutoMapper\Event\PropertyMetadataEvent;
+use AutoMapper\EventListener\MapFromListener;
 use AutoMapper\EventListener\MapToContextListener;
+use AutoMapper\EventListener\MapToListener;
 use AutoMapper\EventListener\Symfony\AdvancedNameConverterListener;
+use AutoMapper\Transformer\CustomTransformer\CustomTransformersRegistry;
 use Symfony\Component\Serializer\NameConverter\AdvancedNameConverterInterface;
 
 return static function (ContainerConfigurator $container) {
@@ -14,6 +18,12 @@ return static function (ContainerConfigurator $container) {
         ->set(MapToContextListener::class)
             ->args([service('automapper.property_info.reflection_extractor')])
             ->tag('kernel.event_listener', ['event' => PropertyMetadataEvent::class, 'priority' => 64])
+        ->set(MapToListener::class)
+            ->args([service(CustomTransformersRegistry::class)])
+            ->tag('kernel.event_listener', ['event' => GenerateMapperEvent::class, 'priority' => 64])
+        ->set(MapFromListener::class)
+            ->args([service(CustomTransformersRegistry::class)])
+            ->tag('kernel.event_listener', ['event' => GenerateMapperEvent::class, 'priority' => 32])
 
         ->set(AdvancedNameConverterListener::class)
             ->args([service(AdvancedNameConverterInterface::class)])
