fa61df19e8a05afc9ad28569e4443fdf96034cd4
Baptiste <baptiste.leduc@gmail.com>
2024-05-06 11:42:53 +0200

    Fix documentation build

--- .github/scripts/build-documentation.sh
+++ .github/scripts/build-documentation.sh
@@ -16,12 +16,12 @@ poetry run mike deploy --push --remote gh-pages dev
 
 # build documentation for 8.x
 git checkout tags/8.2.2
-poetry run mike deploy --push --remote gh-pages --update-aliases 8.2.2 latest
-poetry run mike set-default --push latest
+poetry run mike deploy --push --remote gh-pages --update-aliases 8.2.2
 
 # build documentation for 9.x
-git checkout tags/9.0.0-beta.2
-poetry run mike deploy --push --remote gh-pages 9.0.0-beta.2
+git checkout tags/9.0.0
+poetry run mike deploy --push --remote gh-pages 9.0.0 latest
+poetry run mike set-default --push latest
 
 # clean fake remote
 cd ./.build
