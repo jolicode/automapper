cdbd0a2bfd8555115acf2e8b33823aa827f79520
Grégoire Pineau <lyrixx@lyrixx.info>
2024-07-24 15:25:19 +0200

    Add reproducer - Squash me - Constructor with default paramater array does not work

--- tests/Fixtures/ConstructorWithDefaultValues.php
+++ tests/Fixtures/ConstructorWithDefaultValues.php
@@ -9,7 +9,8 @@ readonly class ConstructorWithDefaultValues
     public function __construct(
         public string $baz,
         public ?int $foo = 1,
-        public int $bar = 0
+        public int $bar = 0,
+        public array $someOtters = [],
     ) {
     }
 }
