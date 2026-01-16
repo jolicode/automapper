e043b74ba30bc1bf57617aae0008128af2b60cbf
Joel Wurtz <joel.wurtz@gmail.com>
2026-01-16 09:54:32 +0100

    feat(symfony): add an array cache for better performance when generating mappers

--- src/Symfony/Bundle/Resources/config/property_info.php
+++ src/Symfony/Bundle/Resources/config/property_info.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\PropertyInfo\Extractor\PhpStanExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoCacheExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 
 return static function (ContainerConfigurator $container) {
@@ -34,11 +37,11 @@ return static function (ContainerConfigurator $container) {
                     '$allowPrivateAccess' => false,
                     '$accessorPrefixes' => [],
                 ])
-            ->set('automapper.property_info.source_type_extractor', PropertyInfoExtractor::class)
+            ->set('automapper.property_info.source_property_info_extractor', PropertyInfoExtractor::class)
                 ->args([
                     '$typeExtractors' => [service('automapper.property_info.phpstan_source_extractor'), service('automapper.property_info.reflection_source_extractor')],
                 ])
-            ->set('automapper.property_info.target_type_extractor', PropertyInfoExtractor::class)
+            ->set('automapper.property_info.target_property_info_extractor', PropertyInfoExtractor::class)
                 ->args([
                     '$typeExtractors' => [service('automapper.property_info.phpstan_target_extractor'), service('automapper.property_info.reflection_target_extractor')],
                 ])
@@ -47,4 +50,27 @@ return static function (ContainerConfigurator $container) {
                     '$accessFlags' => ReflectionExtractor::ALLOW_PUBLIC,
                 ])
     ;
+
+    if (interface_exists(AdapterInterface::class)) {
+        $container->services()
+            ->set('automapper.property_info.cache_pool_extractor', ArrayAdapter::class)
+            ->set('automapper.property_info.cached_source_property_info_extractor', PropertyInfoCacheExtractor::class)
+                ->args([
+                    service('automapper.property_info.source_property_info_extractor'),
+                    service('automapper.property_info.cache_pool_extractor'),
+                ])
+            ->set('automapper.property_info.cached_target_property_info_extractor', PropertyInfoCacheExtractor::class)
+                ->args([
+                    service('automapper.property_info.target_property_info_extractor'),
+                    service('automapper.property_info.cache_pool_extractor'),
+                ])
+            ->alias('automapper.property_info.source_type_extractor', 'automapper.property_info.cached_source_property_info_extractor')
+            ->alias('automapper.property_info.target_type_extractor', 'automapper.property_info.cached_target_property_info_extractor')
+        ;
+    } else {
+        $container->services()
+            ->alias('automapper.property_info.source_type_extractor', 'automapper.property_info.source_property_info_extractor')
+            ->alias('automapper.property_info.target_type_extractor', 'automapper.property_info.target_property_info_extractor')
+        ;
+    }
 };
