d80fa0cb7a207e3e8d4c5c93a380098501f53cfc
Joel Wurtz <joel.wurtz@gmail.com>
2026-07-17 17:14:49 +0200

    fix(bundle): do not register cache warmer with eval loader

--- tests/Bundle/DependencyInjection/AutoMapperExtensionTest.php
+++ tests/Bundle/DependencyInjection/AutoMapperExtensionTest.php
@@ -6,6 +6,7 @@ namespace AutoMapper\Tests\Bundle\DependencyInjection;
 
 use AutoMapper\Loader\FileLoader;
 use AutoMapper\Loader\FileReloadStrategy;
+use AutoMapper\Symfony\Bundle\CacheWarmup\CacheWarmer;
 use AutoMapper\Symfony\Bundle\DependencyInjection\AutoMapperExtension;
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -36,6 +37,27 @@ final class AutoMapperExtensionTest extends AbstractExtensionTestCase
         yield 'Never reload if no conf and no debug' => [[], false, FileReloadStrategy::NEVER];
         yield 'Always reload if no conf and no debug' => [[], true, FileReloadStrategy::ALWAYS];
         yield 'Applies configured reload strategy if provided' => [['reload_strategy' => FileReloadStrategy::NEVER->value], true, FileReloadStrategy::NEVER];
+    }
+
+    public function testEvalLoaderDoesNotRegisterTheCacheWarmer(): void
+    {
+        $this->container->setParameter('kernel.debug', false);
+        $this->load(['loader' => ['eval' => true]]);
+
+        // there is no cache directory with the eval loader: the cache warmer must not be registered,
+        // otherwise the container fails to compile on the missing automapper.cache_dir parameter
+        $this->assertContainerBuilderNotHasService(CacheWarmer::class);
+
+        $this->container->compile();
+    }
+
+    public function testFileLoaderRegistersTheCacheWarmer(): void
+    {
+        $this->container->setParameter('kernel.debug', false);
+        $this->load();
+
+        $this->assertContainerBuilderHasService(CacheWarmer::class);
+        $this->assertContainerBuilderHasParameter('automapper.cache_dir');
     }
 
     protected function getContainerExtensions(): array
